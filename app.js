5dbe7c9a9793d5eb28928cbca50e7409e56565c6
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2024-02-02 23:58:10 +0200

    Add POST endpoint

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const app = express();
 const mongoConnection = require('./configs/mongoConnection');
 const indexRouter = require('./routes/index');
 
+app.set('view engine', 'pug');
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static(__dirname + '/public'));
+
 app.use('/', indexRouter);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on  http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/userModule.js');
+const User = require('../models/userModel.js');
+
+router.get('/', (req, res) => {
+  res.render('createUser');
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+    res.redirect('/');
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
 module.exports = router;
