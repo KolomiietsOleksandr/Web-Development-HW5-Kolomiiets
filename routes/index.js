90f481f390dab7f465320cef5a766598a710d0c6
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2024-02-03 00:21:35 +0200

    Add list of users

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/userModel.js');
 
-router.get('/', (req, res) => {
+router.get('/new', (req, res) => {
   res.render('createUser');
 });
 
-router.post('/', async (req, res) => {
+router.post('/new', async (req, res) => {
   try {
     const { username, email, password } = req.body;
     const newUser = new User({ username, email, password });
@@ -16,5 +16,14 @@ router.post('/', async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
+
+router.get('/', async (req, res) => {
+    try {
+      const users = await User.find();
+      res.render('userList', { users })
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
 
 module.exports = router;
