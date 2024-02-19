c6fb9492fde42df1ccdb50bb9de824d7e9c18208
Oleksandr Kolomiiets <alexcrimson0818@gmail.com>
2024-02-19 23:14:10 +0200

    Connect with Django, convert photo

--- express/handlers/handler.js
+++ express/handlers/handler.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const path = require('path');
-const User = require('../models/userModel.js');
+const axios = require('axios');
 const fs = require('fs');
+const FormData = require('form-data');
+const User = require('../models/userModel.js');
 
 const documentationFilePath = 'documentation.json';
 const documentation = JSON.parse(fs.readFileSync(documentationFilePath, 'utf8'));
@@ -21,28 +23,47 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-const imageFormats = ['jpg', 'jpeg', 'png', 'gif'];
-router.get('/upload', (req, res) => {
-  res.render('uploadFile', { imageFormats });
-});
+const imageFormats = ['jpeg', 'png', 'gif'];
 
-router.post('/upload', upload.single('file'), (req, res) => {
+router.post('/converter/upload', upload.single('file'), async (req, res) => {
   if (!req.file) {
-    return res.redirect('/upload');
+    return res.redirect('/converter/upload');
   }
 
   const fileExtension = path.extname(req.file.originalname).toLowerCase().substr(1);
 
   if (!imageFormats.includes(fileExtension)) {
     fs.unlinkSync(req.file.path);
-    return res.redirect('/upload');
+    return res.redirect('/converter/upload');
   }
 
   const selectedFormat = req.body.imageFormat;
 
-  req.session.success = 'File uploaded successfully';
-  // res.redirect('/');
+  try {
+    const formData = new FormData();
+    formData.append('file', fs.createReadStream(req.file.path));
+    formData.append('file_type', selectedFormat);
+
+    const djangoResponse = await axios.post('http://127.0.0.1:8000/upload/', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+      responseType: 'stream'
+    });
+
+    res.set('Content-Type', djangoResponse.headers['content-type']);
+    res.set('Content-Disposition', djangoResponse.headers['content-disposition']);
+    djangoResponse.data.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.redirect('/converter/upload');
+  }
 });
+
+router.get('/converter/upload', (req, res) => {
+  res.render('uploadFile', { imageFormats });
+});
+
 
 router.get('/help', (req, res) => {
   res.json(documentation);
@@ -64,70 +85,74 @@ router.post('/new', async (req, res) => {
 });
 
 router.get('/', async (req, res) => {
-    try {
-      const users = await User.find();
-      res.render('userList', { users })
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
+  try {
+    const users = await User.find();
+    res.render('userList', { users })
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-  router.get('/:id', async (req, res) => {
-    try {
-      const userId = req.params.id;
-      const user = await User.findById(userId);
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      res.render('userDetail', { user });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
+router.get('/:id', async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
     }
-  });
+    res.render('userDetail', { user });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-  router.patch('/:id', async (req, res) => {
-    try {
-      const userId = req.params.id;
-      const updates = req.body;
-      const options = { new: true };
-      const updatedUser = await User.findByIdAndUpdate(userId, updates, options);
-      if (!updatedUser) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      res.status(200).json(updatedUser);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
+router.patch('/:id', async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const updates = req.body;
+    const options = { new: true };
+    const updatedUser = await User.findByIdAndUpdate(userId, updates, options);
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
     }
-  });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
-  router.delete('/:id', async (req, res) => {
-    try {
-      const userId = req.params.id;
-      const deletedUser = await User.findByIdAndDelete(userId);
-      if (!deletedUser) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      res.status(200).json({ message: 'User deleted successfully' });
-    } catch (error) {
-      res.status(400).json({ message: error.message });
+router.delete('/:id', async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      return res.status(404).json({ message: 'User not found' });
     }
-  });
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
-  router.put('/:id', async (req, res) => {
-    try {
-      const userId = req.params.id;
-      const updatedUser = await User.findByIdAndUpdate(userId, req.body, { new: true });
-      if (!updatedUser) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-      res.status(200).json(updatedUser);
-    } catch (error) {
-      res.status(400).json({ message: error.message });
+router.put('/:id', async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const updatedUser = await User.findByIdAndUpdate(userId, req.body, { new: true });
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
     }
-  });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 
-  router.get('*', (req, res) => {
-    res.status(404).render('error', { message: 'Page not found' });
-  });
+router.get('/converter/upload', (req, res) => {
+  res.render('uploadFile', { imageFormats });
+});
+
+router.get('*', (req, res) => {
+  res.status(404).render('error', { message: 'Page not found' });
+});
 
 module.exports = router;
